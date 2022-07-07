c9b766612da5f58e14ffa5fc6c25b924245d6e8c
Rodrigo Quezada <37853113+qxalla@users.noreply.github.com>
2022-07-06 20:53:12 -0400

    área del trapecio

---
+++ ATDFI102_s2_03_nombregrupo.py
@@ -0,0 +1,6 @@
+#área del trapecio
+base_superior=float(input('Ingresa la longitud en cms de la base superior: '))
+base_inferior=float(input('Ingresa la longitud en cms de la base Inferior: '))
+altura=float(input('Ingresa la altura en: '))
+area=(altura*((base_superior+base_inferior)/2))
+print('El área del trapecio es: %.2f cms2' %(area))
